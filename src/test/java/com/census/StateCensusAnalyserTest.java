b5bdac05a47294ab84c94ca83b02d948897d6906
Babakailash <nathkailash2020@gmail.com>
2021-05-23 13:04:52 +0530

    Welcome To India State Census

--- src/test/java/com/census/StateCensusAnalyserTest.java
+++ src/test/java/com/census/StateCensusAnalyserTest.java
@@ -6,12 +6,12 @@ import java.io.IOException;
 
 public class StateCensusAnalyserTest {
 
-   /* public static final String STATECENSUS_CSVFILE = "C:\\Users\\BABA\\Census_Analyzer\\src\\main\\resources\\IndiaStateCensusData.csv";
+    public static final String STATECENSUS_CSVFILE = "C:\\Users\\BABA\\Census_Analyzer\\src\\main\\resources\\IndiaStateCensusData.csv";
 
 
     @Test
     public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
-        try {
+       /* try {
             int count = StateCensusAnalyser.openCsvBuilder(STATECENSUS_CSVFILE, CSVStateCensus.class);
             System.out.println(count);
             Assertions.assertEquals(29, count);
@@ -19,4 +19,4 @@ public class StateCensusAnalyserTest {
             e.printStackTrace();
         }
     }*/
-}
+    }}
