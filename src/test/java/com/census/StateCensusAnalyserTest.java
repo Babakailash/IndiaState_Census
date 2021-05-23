1d96bfc81312d83689deb81a50b30398b9ead16b
Babakailash <nathkailash2020@gmail.com>
2021-05-23 14:29:08 +0530

    Change:Header Incorrect Return False

--- src/test/java/com/census/StateCensusAnalyserTest.java
+++ src/test/java/com/census/StateCensusAnalyserTest.java
@@ -52,4 +52,14 @@ public class StateCensusAnalyserTest {
 
         }
     }
+
+    @Test
+    public void whenCorrectCensusCSVFile_ButHeaderIncorrect_ShouldReturnFalse() throws IOException {
+        try {
+            int count = StateCensusAnalyser.openCsvBuilder(STATECENSUS_CSVFILE, CSVStateCensus.class);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+            Assertions.assertEquals(CensusAnalyserException.CensusExceptionType.INCORRECT_DATA_ISSUE, e.type);
+        }
+    }
 }
